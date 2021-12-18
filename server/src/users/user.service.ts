6396a4120360fafa55aa0bb7685e9d2edb7cbef0
Cameron Thacker <cthacker@udel.edu>
2021-12-18 12:53:26 -0500

    Added getUser and getAllUsers function calls

--- server/src/users/user.service.ts
+++ server/src/users/user.service.ts
@@ -1,11 +1,20 @@
+import { getMongoManager, MongoEntityManager } from 'typeorm';
 import { Injectable } from "@nestjs/common";
-
-
-
+import { UserEntity } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
 
-    
+    private mongoManager: MongoEntityManager;
 
-}
+    async getAllUsers(): Promise<UserEntity[]> {
+        return await getMongoManager("mongo").find(UserEntity);
+    };
+
+    async getUser(firstName: string): Promise<UserEntity[]> {
+        return await getMongoManager("mongo").find(UserEntity, {
+            first: firstName
+        });
+    }
+
+};
